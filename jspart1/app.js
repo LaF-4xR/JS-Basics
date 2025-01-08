59810225685402f4b9f87fd4e960840cbb0e20cc
Laffy <bidwattam04@gmail.com>
2025-01-08 21:56:53 +0530

    file commited

---
+++ jspart1/app.js
@@ -0,0 +1,11 @@
+let a = 'Red';
+
+if(a == 'Green'){
+    console.log(`You can go. The color is ${a}`);
+}
+else if(a == 'Red'){
+    console.log(`Stop. The color is ${a}`);
+}
+else if(a == 'Yellow'){
+    console.log(`Caution. The color is ${a}`);
+}
