71fe5243a2df27291100ca8f7a754323564a2357
Laffy <bidwattam04@gmail.com>
2025-01-08 22:00:02 +0530

    file commited

---
+++ jspart1/app4.js
@@ -0,0 +1,9 @@
+let arr1 = ['january','july','march','august'];
+console.log(arr1);
+// arr1.shift();
+// arr1.shift();
+// arr1.unshift("june");
+// arr1.unshift("july");
+// console.log(arr1);
+arr1.splice(0,2,"july","june");
+console.log(arr1);
