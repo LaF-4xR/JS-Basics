eb60950ffba52ad9e6b1c198958fd3b432e6c00d
Laffy <bidwattam04@gmail.com>
2025-01-08 21:59:16 +0530

    file commited

---
+++ jspart1/app3.js
@@ -0,0 +1,13 @@
+let word = "aaya";
+if (word[0] == 'a' || word[0] == 'A') 
+    {
+    if (word.length > 3) {
+        console.log("It is a good string");
+    }
+    else {
+        console.log("It is a bad string");
+    }
+}
+else{
+    console.log("It does not start with A or a");
+}
