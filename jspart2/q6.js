8450131f9212fae9c3ca2d5d0cd63b07c7d81fb1
Laffy <bidwattam04@gmail.com>
2025-01-09 20:53:29 +0530

    file commited

---
+++ jspart2/q6.js
@@ -0,0 +1,7 @@
+let n1 = 221;
+let n2 = 422222288821;
+if((n1%10) == (n2%10)){
+    console.log("The numbers have same last digit");
+}else{
+    console.log("The numbers do not have the same last digit")
+}
