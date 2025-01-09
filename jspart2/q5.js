bc077d32682f90304f65f4b444a207263b38c1f8
Laffy <bidwattam04@gmail.com>
2025-01-09 20:52:48 +0530

    file commited

---
+++ jspart2/q5.js
@@ -0,0 +1,12 @@
+let n = 9;
+let n2 = 6;
+let n3 = 7;
+if(n>n2 && n>n3){
+    console.log("Num 1 is the largest");
+}
+else if(n2>n && n2>n3){
+    console.log("Num 2 is the largest");
+}
+else{
+    console.log("Num 3 is the largest");
+}
