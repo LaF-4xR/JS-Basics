9afe0b8827cc8b78d9bab930bcaf883d60a59fb4
Laffy <bidwattam04@gmail.com>
2025-01-09 20:51:23 +0530

    file commited

---
+++ jspart2/q4.js
@@ -0,0 +1,7 @@
+str = "Aplek";
+len = str.length;
+if((str[0] === 'A' || str[0] === 'a') && len>5){
+    console.log("It is a golden string");
+}else{
+    console.log("It is not a golden string");
+}
