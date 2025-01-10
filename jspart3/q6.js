d33d637f039585c3aef432865cd383e6daff8efb
Laffy <bidwattam04@gmail.com>
2025-01-10 21:59:42 +0530

    file commited

---
+++ jspart3/q6.js
@@ -0,0 +1,7 @@
+let arr = ["Minku", 80, 1.10, 1.6, 7, 'k', -9];
+element = -9;
+if(arr.indexOf(element) == -1){
+    console.log(`The respective element is not present in the array`);
+}else{
+    console.log(`The element ${element} is present in the array`);
+}
