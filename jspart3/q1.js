98658178775d895136e13c3051b337316aaeca45
Laffy <bidwattam04@gmail.com>
2025-01-10 21:54:02 +0530

    file commited

---
+++ jspart3/q1.js
@@ -0,0 +1,8 @@
+// let arr = [7, 9, 0, -2];
+// console.log(`Array before updataion: ${arr}`)
+// arr1 = arr.slice(0,3);
+// console.log(`Array after updation: ${arr1}`);
+let arr = [7,9,0,-2]; 
+let n = 3;
+let ans=arr.slice(0,n); 
+console.log(ans);
