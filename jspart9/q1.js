a4e490e164477763212f1ad44c2a0b2911f57047
Laffy <bidwattam04@gmail.com>
2025-01-21 23:44:53 +0530

    file commited

---
+++ jspart9/q1.js
@@ -0,0 +1,6 @@
+let b = document.createElement('button');
+let input = document.createElement('input');
+b.innerText = "Click me";
+
+document.querySelector('body').append(input);
+document.querySelector('body').append(b);
