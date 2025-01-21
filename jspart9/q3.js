a7853a9801ee841f9dc9ee4dd8aa1d862e1a0ed7
Laffy <bidwattam04@gmail.com>
2025-01-21 23:46:39 +0530

    file commited

---
+++ jspart9/q3.js
@@ -0,0 +1,12 @@
+let b = document.createElement('button');
+let input = document.createElement('input');
+b.innerText = "Click me";
+
+document.querySelector('body').append(input);
+document.querySelector('body').append(b);
+
+b.setAttribute("id", "btn");
+input.setAttribute("placeholder", "username");
+
+let btn = document.querySelector("#btn");
+btn.classList.add("btnStyle");
