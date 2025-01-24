22b257d2e7d506b0efca5f730d85292ed2e91679
Laffy <bidwattam04@gmail.com>
2025-01-24 23:35:13 +0530

    file commited

---
+++ jspart11/call_stack1.js
@@ -0,0 +1,13 @@
+function hello(){
+    console.log("Inside hello function");
+    console.log("hello");
+}
+
+function demo(){
+    console.log("Calling hello function");
+    hello();
+}
+
+console.log("Calling demo function");
+demo();
+console.log("done, bye!");
