e6d28c4d0f8ec05f7fa2c9a357406e07853844e0
Laffy <bidwattam04@gmail.com>
2025-01-24 23:48:12 +0530

    file commited

---
+++ jspart11/callback_hell1.js
@@ -0,0 +1,22 @@
+h1 = document.querySelector("h1");
+
+function changeColor(color, delay, nextColorChange){
+    setTimeout(()=>{
+        h1.style.color = color;
+        if(nextColorChange) nextColorChange();
+    }, delay);
+}
+
+changeColor("blue", 1000, () => {
+    changeColor("red", 1000, () => {
+        changeColor("yellow", 1000, () => {
+            changeColor("green", 1000, () => {
+                changeColor("purple", 1000, () => {
+                    changeColor("brown", 1000);
+                });
+            });
+        });
+    });
+});
+
+//Callback Nesting -> Callback hells
