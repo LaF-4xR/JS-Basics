fd7020e7a9a15d0918716ac33bd309bd2b118c0a
Laffy <bidwattam04@gmail.com>
2025-01-25 01:21:42 +0530

    file commited

---
+++ jspart11/promise2.js
@@ -0,0 +1,30 @@
+function savetoDB(data){
+    return new Promise((resolve, reject) =>{
+        let speed = Math.floor(Math.random() * 10) + 1;
+        if(speed > 4){
+            resolve("success: data was saved");
+        } else {
+            reject("failure: weak connection");
+        }
+    }); 
+}
+
+savetoDB("Hello")
+.then((result) => {
+    console.log("data1 saved");
+    console.log("result of promise: ", result);
+    return savetoDB("hello world");
+})
+.then((result) => {
+    console.log("data2 saved");
+    console.log("result of promise: ", result);
+    return savetoDB("HELLO WORLD");
+})
+.then((result) => {
+    console.log("data3 saved");
+    console.log("result of promise: ", result);
+})
+.catch((error) => {
+    console.log("promise was rejected");
+    console.log("error of promise: ", error);
+});
