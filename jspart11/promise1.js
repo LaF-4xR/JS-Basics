d69b7e8e06cc63c1a773450b321eb345387b3d31
Laffy <bidwattam04@gmail.com>
2025-01-26 23:15:04 +0530

    file commited

--- jspart11/promise1.js
+++ jspart11/promise1.js
@@ -38,3 +38,5 @@ colorChange("blue", 1000)
 });
 
 //Promise method is used to write the codes of comeback hells to make it more compact, efficient and more readable and less confusing which can be very easy to understand for the programmer/user.
+
+//The Promise method helps resolve callback hell by making the code more compact, efficient, readable, and easier for programmers/users to understand.
