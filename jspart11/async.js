928c5a6d7688808b546d14d9bd0584e40b3f26af
Laffy <bidwattam04@gmail.com>
2025-01-27 15:54:14 +0530

    file commited

---
+++ jspart11/async.js
@@ -0,0 +1,17 @@
+async function greet(){
+    //throw "Error 404";  //Throw keyword is used to throw some random errors.
+    return "Yoo guys";
+}
+
+greet()
+.then((res)=>{
+    console.log("Promise was resolved");
+    console.log(res);
+})
+.catch((err)=>{
+    console.log("Promise was rejected", err);
+});
+
+// let demo = async () => {
+//     return 1000;
+// }
