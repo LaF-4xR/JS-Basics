0f57c65403c2ef3d9c14cfb5985a68035cc312af
Laffy <bidwattam04@gmail.com>
2025-01-28 22:59:01 +0530

    file commited

--- jspart11/single_threaded.js
+++ jspart11/single_threaded.js
@@ -9,4 +9,4 @@ setTimeout(()=>{
 console.log("Hello World!");
 
 //This is because js can only perform one task at a time and the setTimeout function is used to print something after a delay.
-//So in JS it does not waits it immediately performs the function but the browser waits to print it in the console after the delay.
+//So in JS it does not waits it immediately performs the function but the browser waits to print it in the console after the delay. 
