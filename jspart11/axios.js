9ef591f0fced66f69c3ea096a0c04e6cb68f0e1f
Laffy <bidwattam04@gmail.com>
2025-02-13 00:16:43 +0530

    file commited

--- jspart11/axios.js
+++ jspart11/axios.js
@@ -1,21 +1,30 @@
 let url = "https://catfact.ninja/fact";
 
-let btn = document.querySelector("button");
+document.addEventListener("DOMContentLoaded", function () {
+    let btn = document.querySelector("button");
 
-btn.addEventListener("click", () => {
+    btn.addEventListener("click", async function () {
+        let fact= await getFacts();
+        console.log(fact);
+        let p = document.querySelector("p");
+        p.innerText = fact;
+    });
 
-})
+
+
 
 async function getFacts() {
     try
     {
         let res = await axios.get(url);
         
+        return res.data.fact;
     
-        console.log(res.data);
-        console.log(res.data.fact);
-        console.log(res.data.length);
+        // console.log(res.data);
+        // console.log(res.data.fact);
+        // console.log(res.data.length);
     } catch(e) {
         console.log("Error", e);
     }
 }
+});
